8bc644c21108eb65af251e46f88c06e673352950
francesco_fiorella <francescofio99@gmail.com>
2020-11-13 22:11:33 +0100

    MainActivity.java is updated: button and intent definition, passaggio informazione "HELLO WORLD!" alla secondActivity nell'onClickListener

--- app/src/main/java/com/example/quicklauncher/MainActivity.java
+++ app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.quicklauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // definizione button
+        Button secondActivityBtn = (Button)findViewById(R.id.secondActivityBtn);
+        // onClick
+        secondActivityBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // crea un intent che passa alla secondActivity (definizione intent)
+                Intent startIntent = new Intent(getApplicationContext(), SecondActivity.class);
+                // passa informazioni alla secondActivity, name specifica l'id dell'informazione
+                startIntent.putExtra("org.example.quicklauncher.SOMETHING", "HELLO WORLD!");
+                // esegui l'intent
+                startActivity(startIntent);
+            }
+        });
     }
 }
